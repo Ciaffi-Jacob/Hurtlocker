82ff6fe63f9eb747ffb311dbaa5260b049ecaacd
Jacob C <jacobc@Zipcoders-MacBook-Pro-3.local>
2023-04-17 15:21:16 -0400

    methods and sudo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,35 +5,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    private HashMap hashMap;
+    private HashMap hashMap; //buckets will track how many of each thing for count
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
-
-
-
-
-        //filter
-        String patternString = "##";
-        Pattern pattern = Pattern.compile(patternString);
-        Matcher matcher = pattern.matcher(result);
-        for (int i = 0; matcher.find(); i++) {
-
-            System.out.println(
-                    matcher.
-
-            );
-        }
-        //recollect
-        //regex to find
-        //regex to print
         return result;
     }
+    public String pattern(String s, String splitter){//take dataToString and what will be split on
+        //pattern-matcher to break up data
+        Pattern pattern = Pattern.compile()
+        return  null;
+    }
+    public String split(String s){//takes broken up data and breaks up further with regex [;:^@] etc
+        return null;
+        //send broken matcher to store
+    }
+    public void store(){//takes broken up data and puts into map
 
+    }
+    public void format(){//takes map and formats for print
+
+    }
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
 
+        System.out.println(output);
     }
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -12,12 +12,12 @@ public class TestMain {
                 "============= \t \t =============\n" +
                 "Price: \t 3.23\t\t seen: 1 times\n" +
                 "-------------\t\t -------------";
-        String actual = split("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016");
+        String actual = (new Main()).split("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016");
 
         Assert.assertEquals(expected, actual);
     }
     @Test
-    public void testReadRawDataToString(){
+    public void testReadRawDataToString() throws Exception {
         String expected = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
         String actual = (new Main()).readRawDataToString();
         Assert.assertEquals(expected, actual);
@@ -26,7 +26,7 @@ public class TestMain {
     public void testPattern(){//takes string (result)
         Pattern p = Pattern.compile("##");
         Matcher m = p.matcher("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##");
-        String actual = pattern("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##", "##");
+        String actual = (new Main()).pattern("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##", "##");
 
         Assert.assertEquals(m.toString(), actual);
     }
