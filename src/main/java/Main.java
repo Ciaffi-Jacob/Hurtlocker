54b3aa596dc667e212e83db2bf1fa6347a43144a
Jacob C <jacobc@Zipcoders-MacBook-Pro-3.local>
2023-04-17 12:03:46 -0400

    wrote tests for read raw and split - also some sudo ish to understand

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+
+
+
+
+        //filter
+        String patternString = "##";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(result);
+        for (int i = 0; matcher.find(); i++) {
+
+            System.out.println(
+                    matcher.
+
+            );
+        }
+        //recollect
+        //regex to find
+        //regex to print
         return result;
     }
 
